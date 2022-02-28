7ca4747e8dc2314f13994be8cda924d4b6aa9685
alfred <alfredjin@163.com>
2022-02-28 13:30:56 +0800

    security

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "alfred", password: "alfred", except: [:index, :show]
+  
+
   def index
     @articles = Article.all
   end
